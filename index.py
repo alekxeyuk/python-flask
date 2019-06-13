a4ecb4abdfbbb5787ac5582c54a792b7bf300b29
alekxeyuk <alekx@alumni.nottingham.ac.uk>
2019-06-13 19:25:38 +1000

    added vue.js

--- index.py
+++ index.py
@@ -1,9 +1,15 @@
-from flask import Flask, Response, __version__
-app = Flask(__name__)
-source = 'https://github.com/zeit/now-examples/tree/master/python-flask'
-css = '<link rel="stylesheet" href="/css/style.css" />'
+from flask import Flask, Response, request, __version__
+import requests
+app = Flask(__name__, static_url_path='')
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Visit the <a href='./about'>about</a> page or view the <a href='%s'>source code</a>.</p>" % (css, source), mimetype='text/html')
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/image/<path:path>')
+def image(path):
+    r = requests.get(path + f"?e={request.args.get('e')}&m={request.args.get('m')}")
+    def generate():
+        for chunk in r.iter_content(chunk_size=128):
+            yield chunk
+    return Response(generate(), mimetype='image/jpeg')
---
+++ static/main.js
@@ -0,0 +1,43 @@
+new Vue({
+    el: "#app",
+    data: {
+        tweets: [],
+        page: 1,
+        resourse_url: '',
+        loading: false
+    },
+    methods: {
+        onScroll: function(event){
+            var wrapper = event.target,
+                list = wrapper.firstElementChild
+            var scrollTop = wrapper.scrollTop,
+                wrapperHeight = wrapper.offsetHeight,
+                listHeight = list.offsetHeight
+
+            var diffHeight = listHeight - wrapperHeight
+
+            if(diffHeight <= scrollTop && !this.loading) {
+                this.load()
+            }
+        },
+        load: function(){
+            this.loading = true
+            this.resourse_url = `https://capi-v2.sankakucomplex.com/posts?lang=english&page=${this.page}&limit=40&tags=order:quality+hide_posts_in_books:never+trap+rating:s`
+            this.$http.get(this.resourse_url).then((responce) => {
+
+                var json = responce.data
+                console.log(json)
+                this.tweets = this.tweets.concat(json)
+                this.page += 1
+                this.loading = false
+
+            }, (error) => {
+                console.log(error)
+                this.loading = false
+            })
+        }
+    },
+    created() {
+        this.load()
+    },
+})
