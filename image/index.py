77974d65bfe65f0e1d82591575fadc9721f93d6c
alekxeyuk <alekx@alumni.nottingham.ac.uk>
2019-06-13 20:48:36 +1000

    Update index.py

--- image/index.py
+++ image/index.py
@@ -1,11 +1,14 @@
 from flask import Flask, Response, request, __version__
 import requests
 app = Flask(__name__)
-
+proxies = {
+    'http': 'http://188.217.121.242:8118',
+    'https': 'https://188.217.121.242:8118'
+}
 @app.route('/<path:path>')
 def catch_all(path):
-    print(requests.get("https://capi-v2.sankakucomplex.com/posts").content)
-    return Response(requests.get("https://capi-v2.sankakucomplex.com/posts").content, mimetype='application/json')
+    print(requests.get("https://capi-v2.sankakucomplex.com/posts", proxies=proxies).content)
+    return Response(requests.get("https://capi-v2.sankakucomplex.com/posts", proxies=proxies).content, mimetype='application/json')
     # print(request.args)
     # r = requests.get(path[6:])
     # def generate():
