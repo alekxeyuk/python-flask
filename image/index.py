bba37597b4b3efb78bd428800ed063d1956a869f
alekxeyuk <alekx@alumni.nottingham.ac.uk>
2019-06-13 20:13:58 +1000

    how the f does that work

--- image/index.py
+++ image/index.py
@@ -2,7 +2,6 @@ from flask import Flask, Response, request, __version__
 import requests
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     r = requests.get(path + f"?e={request.args.get('e')}&m={request.args.get('m')}")
