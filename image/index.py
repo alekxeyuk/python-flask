84f44b12fd7ce528223c20d292413e30d557159d
alekxeyuk <alekx@alumni.nottingham.ac.uk>
2019-06-13 19:51:09 +1000

    image url

---
+++ image/index.py
@@ -0,0 +1,12 @@
+from flask import Flask, Response, request, __version__
+import requests
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    r = requests.get(path + f"?e={request.args.get('e')}&m={request.args.get('m')}")
+    def generate():
+        for chunk in r.iter_content(chunk_size=128):
+            yield chunk
+    return Response(generate(), mimetype='image/jpeg')
--- index.py
+++ index.py
@@ -1,16 +1,7 @@
-from flask import Flask, Response, request, __version__
-import requests
+from flask import Flask, Response, __version__
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return app.send_static_file('index.html')
-
-@app.route('/image/<path:path>')
-def image(path):
-    r = requests.get(path + f"?e={request.args.get('e')}&m={request.args.get('m')}")
-    def generate():
-        for chunk in r.iter_content(chunk_size=128):
-            yield chunk
-    return Response(generate(), mimetype='image/jpeg')
