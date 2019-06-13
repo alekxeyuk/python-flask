75dcecf4a6031a7ebd39345d15050cb2152f1e3b
alekxeyuk <alekx@alumni.nottingham.ac.uk>
2019-06-13 20:27:51 +1000

    Update index.py

--- image/index.py
+++ image/index.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 @app.route('/<path:path>')
 def catch_all(path):
+    print(path)
+    print(request.args)
     r = requests.get(path[6:] + f"?e={request.args.get('e')}&m={request.args.get('m')}")
     def generate():
         for chunk in r.iter_content(chunk_size=128):
