7993ae63d8dd706342420b5473116f56cf7ddefa
alekxeyuk <alekx@alumni.nottingham.ac.uk>
2019-06-13 19:46:15 +1000

    Update index.py

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return app.send_static_file('/index.html')
+    return app.send_static_file('index.html')
 
 @app.route('/image/<path:path>')
 def image(path):
