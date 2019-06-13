c2570e9dcde97fb437b6b6e466d1cd798707c2c9
alekxeyuk <alekx@alumni.nottingham.ac.uk>
2019-06-13 19:34:24 +1000

    Update index.py

--- index.py
+++ index.py
@@ -2,8 +2,9 @@ from flask import Flask, Response, request, __version__
 import requests
 app = Flask(__name__, static_url_path='static')
 
-@app.route('/')
-def root():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
     return app.send_static_file('static/index.html')
 
 @app.route('/image/<path:path>')
