251103e923339a98d869e84f13c4730c68b1f148
LeLuCh <alekx@alumni.nottingham.ac.uk>
2020-04-23 20:46:06 +1000

    update

--- index.py
+++ index.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask, Response, __version__
+from flask import jsonify
+from flask_pymongo import PyMongo
+
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return app.send_static_file('index.html')
+    # return app.send_static_file('index.html')
+    return jsonify({'result': os.getenv('MONGODB_URI')})
