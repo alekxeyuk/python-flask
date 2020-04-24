9637668d56e99757117a026bb687dc5227df51c2
LeLuCh <alekx@alumni.nottingham.ac.uk>
2020-04-24 15:29:24 +1000

    C O R S

--- index.py
+++ index.py
@@ -6,11 +6,13 @@ import qrcode
 import requests
 from bson import json_util
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 from PIL import Image, ImageDraw
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv('MONGODB_URI')[1:-1] if os.path.isfile('now.json') else os.getenv('MONGODB_URI')
+CORS(app)
 mongo = PyMongo(app)
 ses = requests.Session()
 ses.headers.update({"X-Device-Token": os.getenv('DTF_TOKEN'), "x-this-is-csrf": "THIS IS SPARTA!"})
