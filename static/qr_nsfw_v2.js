ba3ba6b207735879b66be4fee93e1e2f0ab6ca8f
LeLuCh <alekx@alumni.nottingham.ac.uk>
2020-05-10 11:15:18 +1000

    Update qr_nsfw_v2.js

--- static/qr_nsfw_v2.js
+++ static/qr_nsfw_v2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         QR-NSFW
 // @namespace    http://dtf.ru/
-// @version      2.1.0
+// @version      2.1.1
 // @description  Watch NSFW content on DTF using qr-codes magic!
 // @author       Prostagma?
 // @author       Zhenya Sokolov
