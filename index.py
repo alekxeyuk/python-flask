3879c93a82b2285396e1d4d274be88354de7e28b
LeLuCh <alekx@alumni.nottingham.ac.uk>
2020-10-09 14:31:03 +0700

    Update index.py

--- index.py
+++ index.py
@@ -134,8 +134,11 @@ def qrcodes_generate():
                 qr_code = generate_qr_code(bg_size, qr_data)
                 dtf_response = ses.post('https://api.dtf.ru/v1.9/uploader/upload', files={f'file_0': ('file.png', qr_code.getbuffer(), 'image/png')}).json()
                 # fucking around new dtf CDN
-                dirty_hack = ses.get(f"https://leonardo.osnova.io/{dtf_response['result'][0]['data']['uuid']}", stream=True)
+                print('aaa 1')
+                dirty_hack = requests.get(f"https://leonardo.osnova.io/{dtf_response['result'][0]['data']['uuid']}/")
+                print('aaa 2')
                 uuid_for_db = ses.post('https://api.dtf.ru/v1.9/uploader/upload', files={f'file_0': ('file.png', dirty_hack.content, 'image/png')}).json()['result'][0]['data']['uuid']
+                print('aaa 3')
                 del dirty_hack
                 # stop fucking
                 qrify_result_list.append({
